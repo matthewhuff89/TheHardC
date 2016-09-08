24013ac01e9835652e3aa3d88d085a26cbf235d7
Matthew D. Huff <matthewhuff89@gmail.com>
2016-09-08 11:20:25 -0400

    Fixed some errors, created first Makefile for ex2

---
+++ ex2/ex1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/* This is a comment. */
+int main(int argc, char *argv[])
+{
+    int distance = 100;
+    
+    // this is also a comment
+    printf("You are %d miles away.\n", distance);
+    printf("Why are you so far away?\n");
+    printf("What are you running from?\n");
+    printf("Come closer...\n");
+    printf("There's no escaping me, so don't even try...\n");
+
+    return 0;
+}
+
+
+
+
+
+
