80427efe4f08fd0fa7689f9420c362fc00a6efa2
Matthew D. Huff <matthewhuff89@gmail.com>
2016-09-08 12:26:39 -0400

    Way too deep in format sequences

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -3,9 +3,15 @@
 int main(){
     int age = 10;
     int height = 72;
+    char quantity = '1';
+    char quantities[3] = "10";
+    int bros = 5;
 
     printf("I am %d years old \n", age);
     printf("I am %d inches tall\n", height);
+    printf("I am %c bird.\n", quantity);
+    printf("I am %s birds.\n", quantities);
+    printf("I am %i brothers deep outchea.\n", bros);
 
     return 0;
 }
