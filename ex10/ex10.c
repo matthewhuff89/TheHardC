959fb31e12e2c002310a4d82b81b52b113dc292a
Matthew D. Huff <matthewhuff89@gmail.com>
2016-09-09 15:26:33 -0400

    Done with exercise 10

---
+++ ex10/ex10.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    if(argc != 2) {
+        printf("ERROR: You need one argument.\n");
+        // this is how you abort a program.
+        return 1;
+    }
+    
+    int i = 0;
+    char letter;
+    for(i = 0, letter = argv[1][i]; argv[1][i] != '\0'; i++){
+        printf("%d\n", letter);
+
+        switch(letter){
+            //fallthrough
+            case 'a':
+            case 'A':
+                printf("%d: 'A'\n", i);
+                break;
+
+            //fall through
+            case 'e':
+            case 'E':
+                printf("%d: 'E'\n", i);
+                break;
+
+            // fall through
+            case 'i':
+            case 'I':
+                printf("%d: 'I'\n", i);
+                break;
+            
+            // fall through
+            case 'o':
+            case 'O':
+                printf("%d: 'O'\n", i);
+                break;
+            
+             // fall through
+            case 'u':
+            case 'U':
+                printf("%d: 'U'\n", i);
+                break;
+            
+            // fall through
+            case 'y':
+            case 'Y':
+                // why i > 2? is this a bug?
+                if(i > 2){
+                    // it's only sometimes Y
+                    printf("%d: 'Y'\n", i);
+                }
+                break;
+            
+            default:
+                printf("%d: %c is not a vowel\n", i, letter);
+        }
+    }
+
+    return 0;
+}
