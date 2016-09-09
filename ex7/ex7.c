5bc9d3a35c9f15b871ba2b131da6342652b34e93
Matthew D. Huff <matthewhuff89@gmail.com>
2016-09-09 10:41:38 -0400

    Finished with Exercise 7

--- ex7/ex7.c
+++ ex7/ex7.c
@@ -2,14 +2,14 @@
 
 int main(int argc, char *argv[]){
     int distance = 100;
-    float power = 2.345f;
-    double super_power = 56789.4532;
+    float power = 2.345;
+    double super_power = 56789;
     char initial = 'A';
     char first_name[] = "Zed";
     char last_name[] = "Shaw";
 
-    printf("You are %d miles away.\n", distance);
-    printf("You have %f levels of power.\n", power);
+    printf("You are %d miles away.\n", distance+10);
+    printf("You have %f levels of power.\n", power+20);
     printf("You have %f awesome super powers.\n", super_power);
     printf("I have an initial %c.\n", initial);
     printf("I have a first name %s.\n", first_name);
@@ -20,9 +20,9 @@ int main(int argc, char *argv[]){
     double bug_rate = 1.2;
 
     printf("You have %d bugs at the imaginary rate of %f.\n", 
-           bugs, bug_rate );
+           bugs*2, bug_rate );
     
-    long universe_of_defects = 1L * 1024L * 1024L * 1024L;
+    unsigned long universe_of_defects = 2048L * 1024L * 1024L * 1024L * 1024L * 1024L;
     printf("The entire universe has %ld bugs.\n", universe_of_defects);
     
     double expected_bugs = bugs * bug_rate;
@@ -35,6 +35,7 @@ int main(int argc, char *argv[]){
     char nul_byte = '\0';
     int care_percentage = bugs * nul_byte;
     printf("Which means you should care %d%%.\n", care_percentage);
+    printf("Take a byte out of crime, you %s.\n", nul_byte);
 
     return 0;
 }
