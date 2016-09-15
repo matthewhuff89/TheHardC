503319f4f47093b6453de7bfef590d2f410a4687
Matthew D. Huff <matthewhuff89@gmail.com>
2016-09-15 13:11:55 -0400

    Finished the extra credit, renamed it is as ex16.c because i was too lazy to mess with my makefile

--- ex16/ex16.c
+++ ex16/ex16.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
+#include <string.h>
 #include <assert.h>
 #include <stdlib.h>
-#include <string.h>
 
 struct Person {
     char *name;
@@ -10,56 +10,31 @@ struct Person {
     int weight;
 };
 
-struct Person *Person_create(char *name, int age, int height, int weight) {
-    struct Person *who = malloc(sizeof(struct Person));
-    assert(who != NULL);
+struct Person Person_create(char *name, int age, int height, int weight) {
+    struct Person x;
+    x.name = name;
+    x.age = age;
+    x.height = height;
+    x.weight = weight;
+    return x;
+};
 
-    who->name = strdup(name);
-    who->age = age;
-    who->height = height;
-    who->weight = weight;
-
-    return who;
-}
-
-void Person_destroy(struct Person *who) {
-    assert(who != NULL);
-    free(who->name);
-    free(who);
-}
-
-void Person_print(struct Person *who) {
-    printf("Name: %s\n", who->name);
-    printf("Age: %d\n", who->age);
-    printf("Height: %d\n", who->height);
-    printf("Weight: %d\n", who->weight);
+void Person_print(struct Person dude) {
+    printf("Name: %s\n", dude.name);
+    printf("Age: %d\n", dude.age);
+    printf("Height: %d\n", dude.height);
+    printf("Weight: %d\n", dude.weight);
 }
 
 int main(int argc, char *argv[]) {
-    // make two people structures
-    struct Person *joe = Person_create("Joe Alex", 32, 64, 140);
-    struct Person *frank = Person_create("Frank Blank", 20, 72, 180);
+    struct Person murok = Person_create("Murok Crunkstorm", 27, 82, 190);
+    struct Person agora = Person_create("Agora Crunkstorm", 27, 76, 170);
 
-    // print them out and where they are in memory
-    printf("Joe is at memory location %p:\n", joe);
-    Person_print(joe);
+    printf("Murok is at memory location %p:\n", &murok);
+    printf("Agora is at memory location %p:\n", &agora);
 
-    printf("Frank is at memory location %p:\n", frank);
-    Person_print(frank);
+    Person_print(murok);
+    Person_print(agora);
 
-    // make everyone age 20 years and print them again
-    joe->age +=20;
-    joe->height -= 2;
-    joe->weight += 40;
-    Person_print(joe);
-
-    frank->age += 20;
-    frank->weight += 20;
-    Person_print(frank);
-
-    // destroy them both so we clean up
-    Person_destroy(joe);
-    Person_destroy(frank);
-    
     return 0;
 }
